ea4d48dba011221bc47e36f4cb9f8d054936e45d
kmakantasis <kostas@kostas-Z68AP-D3.(none)>
2014-03-29 18:50:23 +0200

    Added weighted samples SVM function

--- kmClassification.py
+++ kmClassification.py
@@ -1,21 +1,38 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import pylab as pl
+import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.ensemble import ExtraTreesClassifier
 
 
-def SVMs(trainData, testData, trainLabels, testLabels):
-    clf = svm.SVC()
-    clf.fit(trainData, trainLabels)
-    predictions = clf.predict(testData)
+def SVMs(trainData, testData, trainLabels, testLabels, representatives):
+    repLabels = np.concatenate((np.zeros(100,), (np.ones(100,))), axis=0)
+    
+    trainData = np.concatenate((trainData, representatives), axis=0)
+    trainLabels = np.concatenate((trainLabels, repLabels), axis=0)
+    sample_weight_representatives = np.ones(len(trainData))
+    sample_weight_representatives[1401:] *= 5
+    
+    clf_weights = svm.SVC()
+    clf_weights.fit(trainData, trainLabels, sample_weight=sample_weight_representatives)
 
-    pred_err = [i for i in range(len(predictions)) if predictions[i] != testLabels[i]]
-    pred_err_rat = len(pred_err)/float(len(predictions)) 
+    clf_no_weights = svm.SVC()
+    clf_no_weights.fit(trainData, trainLabels)
+
+    predictions_weights = clf_weights.predict(testData)
+    predictions_no_weights = clf_no_weights.predict(testData)
+
+    pred_err_weights = [i for i in range(len(predictions_weights)) if predictions_weights[i] != testLabels[i]]
+    pred_err_rat_weights = len(pred_err_weights)/float(len(predictions_weights)) 
     
-    return predictions, pred_err_rat
+    pred_err_no_weights = [i for i in range(len(predictions_no_weights)) if predictions_no_weights[i] != testLabels[i]]
+    pred_err_rat_no_weights = len(pred_err_no_weights)/float(len(predictions_no_weights)) 
     
+    return predictions_weights, predictions_no_weights, pred_err_rat_weights, pred_err_rat_no_weights
+    
+   
+   
 def FeaturesImportance(trainData, trainLabels):
     forest = ExtraTreesClassifier(n_estimators=250, random_state=0)
     forest.fit(trainData, trainLabels)
@@ -30,17 +47,18 @@ def FeaturesImportance(trainData, trainLabels):
         print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
 
     # Plot the feature importances of the forest
-    pl.figure()
-    pl.title("Feature importances")
-    pl.bar(range(16), importances[range(16)], color="r", align="center")
-    pl.xticks(range(16), [r'$x_1$', r'$x_2$', r'$x_3$', r'$x_4$', r'$x_5$',
+    plt.figure()
+    plt.title("Feature importances")
+    plt.bar(range(16), importances[range(16)], color="r", align="center")
+    plt.xticks(range(16), [r'$x_1$', r'$x_2$', r'$x_3$', r'$x_4$', r'$x_5$',
                           r'$x_6$', r'$x_7$', r'$x_8$', r'$x_9$', r'$x_{10}$', 
                           r'$x_{11}$', r'$x_{12}$', r'$x_{13}$', r'$x_{14}$', r'$x_{15}$', 
                           r'$x_{16}$'])
-    pl.yticks([0.0, 0.05, 0.10, 0.15, 0.20, 0.25], [r'$0.00$', r'$0.05$', r'$0.10$', r'$0.15$', r'$0.20$', r'$0.25$'])  
-    pl.xlabel('Features')
-    pl.ylabel('Importance')
-    pl.xlim([-1, 16])
-    pl.show()
+    plt.yticks([0.0, 0.05, 0.10, 0.15, 0.20, 0.25], [r'$0.00$', r'$0.05$', r'$0.10$', r'$0.15$', r'$0.20$', r'$0.25$'])  
+    plt.xlabel('Features')
+    plt.ylabel('Importance')
+    plt.xlim([-1, 16])
+    plt.show()
     
     return importances
+    
